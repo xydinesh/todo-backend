23f93e7e008e69ec23ecd4f2933912198aebdf3f
Dinesh Weerapura <xydinesh@gmail.com>
2025-05-21 21:47:15 -0600

    adding cors handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import router from "./routes";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/items", router);
 
